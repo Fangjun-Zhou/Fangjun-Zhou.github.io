43638148295740529c662dc7d171a01abf425630
Fangjun Zhou <fzhou48@wisc.edu>
2022-09-21 11:20:56 -0500

    Update: ECSY Demo

--- scripts/ecsy/src/Main.ts
+++ scripts/ecsy/src/Main.ts
@@ -1,17 +1,38 @@
 import { World } from "ecsy";
 import { ValueData } from "./DataComponents/ValueData";
 
-// Create the main world.
-let mainWorld = new World({
-  entityPoolSize: 10000,
-});
+const main = () => {
+  // 1. World
+  // 1.1 Create a world
 
-const valueData: ValueData = new ValueData();
-console.log(valueData.intVal);
-console.log(valueData.strVal);
+  // Create the main world.
+  let mainWorld = new World({
+    entityPoolSize: 10000,
+  });
 
-valueData.intVal += 10;
-valueData.strVal = "Hello World!";
+  // 2. Components
+  // 2.1 Create a component
+  // 2.2 Change the value of the component
 
-console.log(valueData.intVal);
-console.log(valueData.strVal);
+  let debugTextArea = document.getElementById("debug1") as HTMLTextAreaElement;
+  let debugButton = document.getElementById(
+    "debugButton1"
+  ) as HTMLButtonElement;
+
+  // Create a new component.
+  const valueData: ValueData = new ValueData();
+  debugTextArea.innerHTML +=
+    "Initial ValueData: " + JSON.stringify(valueData) + "\n";
+
+  const ChangeValueData = () => {
+    // Change the value of the component.
+    valueData.intVal += 10;
+    valueData.strVal = "Hello World!";
+    debugTextArea.innerHTML +=
+      "Current ValueData: " + JSON.stringify(valueData) + "\n";
+  };
+
+  debugButton.addEventListener("click", ChangeValueData);
+};
+
+window.onload = main;
