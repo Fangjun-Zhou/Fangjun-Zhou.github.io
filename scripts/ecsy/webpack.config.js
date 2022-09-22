c4d81c7390b441a61f21819585b704aed9b384fc
Fangjun Zhou <fzhou48@wisc.edu>
2022-09-22 12:46:04 -0500

    Update: Webpack Config

--- scripts/ecsy/webpack.config.js
+++ scripts/ecsy/webpack.config.js
@@ -1,6 +1,14 @@
 const path = require("path");
 
 module.exports = {
+  context: path.resolve(__dirname),
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "build"),
+    },
+    compress: true,
+    port: 8080,
+  },
   mode: "development",
   entry: "./src/Main.ts",
   devtool: "inline-source-map",
