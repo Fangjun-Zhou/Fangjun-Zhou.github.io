680a490f7dbfb03977e760379757a3fe77113ee9
Fangjun Zhou <fzhou48@wisc.edu>
2022-09-22 18:03:40 -0500

    Update: DebugRenderSystem

--- scripts/ecsy/src/Demo2.ts
+++ scripts/ecsy/src/Demo2.ts
@@ -2,7 +2,8 @@ import { World } from "ecsy";
 import { Transform2DData } from "./DataComponents/Demo2/Transform2DData";
 import { ClearCanvasSystem } from "./Systems/ClearCanvasSystem";
 import { DebugRenderSystem } from "./Systems/DebugRenderSystem";
-import { LoadSceneSystem } from "./Systems/LoadSceneSystem";
+import { DebugTag } from "./TagComponents/Demo2/DebugTag";
+import { Vector2 } from "./Utils/Vector2";
 
 const world = new World({
   entityPoolSize: 10000,
@@ -17,18 +18,31 @@ export const demo2 = () => {
   // Get main canvas.
   let mainCanvas = document.getElementById("mainCanvas");
 
+  // Return if canvas is not found.
+  if (!mainCanvas) {
+    return;
+  }
+
   // Register all components.
-  world.registerComponent(Transform2DData);
+  world.registerComponent(Transform2DData).registerComponent(DebugTag);
 
   // Register all systems.
   world
-    .registerSystem(LoadSceneSystem)
     .registerSystem(ClearCanvasSystem, {
       canvas: mainCanvas,
     })
     .registerSystem(DebugRenderSystem, {
       canvas: mainCanvas,
     });
+
+  // TODO: Remove this debug entity.
+  // Added a test entity.
+  world
+    .createEntity()
+    .addComponent(Transform2DData, {
+      position: new Vector2(250, 250),
+    })
+    .addComponent(DebugTag);
 
   // Start main loop.
   requestAnimationFrame(mainUpdate);
--- scripts/ecsy/src/Systems/DebugRenderSystem.ts
+++ scripts/ecsy/src/Systems/DebugRenderSystem.ts
@@ -1,10 +1,14 @@
 import { Attributes, System, SystemQueries } from "ecsy";
 import { Transform2DData } from "../DataComponents/Demo2/Transform2DData";
+import { DebugTag } from "../TagComponents/Demo2/DebugTag";
+
+const arrowLength = 50;
+const arrowWidth = 2;
 
 export class DebugRenderSystem extends System {
   static queries: SystemQueries = {
     transformEntities: {
-      components: [Transform2DData],
+      components: [Transform2DData, DebugTag],
     },
   };
 
@@ -30,8 +34,42 @@ export class DebugRenderSystem extends System {
       this.canvasContext.save();
       this.canvasContext.translate(x, y);
 
+      // Draw a circle.
+      this.canvasContext.fillStyle = "green";
+      this.canvasContext.beginPath();
+      this.canvasContext.arc(0, 0, 5, 0, 2 * Math.PI);
+      this.canvasContext.fill();
+
+      // Draw a red arrow for positive X axis.
+      this.canvasContext.beginPath();
+      this.canvasContext.moveTo(0, 0);
+      this.canvasContext.strokeStyle = "red";
+      this.canvasContext.lineWidth = arrowWidth;
+      this.canvasContext.lineTo(arrowLength - 10, 0);
+      this.canvasContext.stroke();
+      // Draw a red arrow top for positive X axis.
+      this.canvasContext.beginPath();
       this.canvasContext.fillStyle = "red";
-      this.canvasContext.arc(0, 0, 20, 0, 2 * Math.PI);
+      this.canvasContext.moveTo(arrowLength, 0);
+      this.canvasContext.lineTo(arrowLength - 10, 5);
+      this.canvasContext.lineTo(arrowLength - 10, -5);
+      this.canvasContext.closePath();
+      this.canvasContext.fill();
+
+      // Draw a blue arrow for positive Y axis.
+      this.canvasContext.beginPath();
+      this.canvasContext.moveTo(0, 0);
+      this.canvasContext.strokeStyle = "blue";
+      this.canvasContext.lineWidth = arrowWidth;
+      this.canvasContext.lineTo(0, arrowLength - 10);
+      this.canvasContext.stroke();
+      // Draw a blue arrow top for positive Y axis.
+      this.canvasContext.beginPath();
+      this.canvasContext.fillStyle = "blue";
+      this.canvasContext.moveTo(0, arrowLength);
+      this.canvasContext.lineTo(5, arrowLength - 10);
+      this.canvasContext.lineTo(-5, arrowLength - 10);
+      this.canvasContext.closePath();
       this.canvasContext.fill();
 
       this.canvasContext.restore();
